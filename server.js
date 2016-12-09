26d3858782940e51d88c58b611bf6a55f9c590b7
Steve Chronister <stephen.g.chronister@gmail.com>
2016-12-09 08:37:22 -0600

    updated port

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ var friendsArray = require("./app/data/friends.js");
 // Sets up the Express App
 // =============================================================
 
-var PORT = process.env.port || 3000;
-
+var PORT = process.env.port;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
